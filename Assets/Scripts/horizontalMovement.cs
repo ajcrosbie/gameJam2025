88de46508f57425808b8f15e5e002430940de08f
ajcrosbie <aaron.j.crosbie@gmail.com>
2025-02-08 19:47:01 +0000

    fixed the horizontalMovement

--- Assets/Scripts/horizontalMovement.cs
+++ Assets/Scripts/horizontalMovement.cs
@@ -1,5 +1,13 @@
-class HorizontalMovement : Power
+using UnityEngine;
+
+public class HorizontalMovement : Power
 {
+    // Initialize method to set up the power (since constructors don't work with MonoBehaviour)
+    public void Initialize(int max, bool useable)
+    {
+        base.Initialize(max, useable);  // Call the base Power class initializer
+    }
+
     public override float getMagnitude()
     {
         return energyStored * 5f;
