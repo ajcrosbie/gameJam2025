684c2bea3499b562a1a1321aa5efa61dac061800
ajcrosbie <aaron.j.crosbie@gmail.com>
2025-02-09 12:48:47 +0000

    added left battery

--- Assets/Scripts/Battery.cs
+++ Assets/Scripts/Battery.cs
@@ -5,6 +5,7 @@ public class Battery : MonoBehaviour
     public int maxBatteries;   // Total available energy
     public Power[] powers;     // Array of powers
     private int energyPool;    // Current energy in the battery
+    private GUIStyle currentStyle;
 
     public void InitializeBattery(int powerSlots, bool[] isOnList)
     {
@@ -46,9 +47,10 @@ public class Battery : MonoBehaviour
         {
             if (powers[i] != null)
             {
-                powers[i].drawBars(i, i);  // PowerTypeOffset = 0 since only one type for now TODO: No longer 0
+                powers[i].drawBars((powers.Length- i), (powers.Length- i));  // PowerTypeOffset = 0 since only one type for now TODO: No longer 0
             }
         }
+        drawBars(1,2);
     }
 
     public bool addPower(int index)
@@ -83,5 +85,49 @@ public class Battery : MonoBehaviour
         }
 
         return false;
+    }
+    public void drawBars(int powerIndex, int powerTypeOffset)
+    {
+        int barWidth = 40;   // Make the bars wider
+        int barHeight = 10;  // Short bars for horizontal layout
+        int spacing = 3;     // Small spacing between bars
+        int verticalSpacing = 15;  // Space between power levels (closer together)
+        int horizontalSpacing = 50; // Space between different power types
+
+        // Align to the top-right corner of the screen
+        int startX = 30 + (0) * horizontalSpacing;  // Moves power types to the left
+        int startY = 10;  // Stacks batteries downward
+
+        for (int i = 0; i < maxBatteries; i++)
+        {
+            Rect barRect = new Rect(startX, startY + i * (barHeight + spacing), barWidth, barHeight);
+            Color barColor = (i < energyPool) ? Color.green : Color.grey;
+
+            drawBar(barRect, barColor);
+        }
+    }
+
+    Texture2D MakeTex( int width, int height, Color col )
+    {
+        Color[] pix = new Color[width * height];
+        for( int i = 0; i < pix.Length; ++i )
+        {
+            pix[ i ] = col;
+        }
+        Texture2D result = new Texture2D( width, height );
+        result.SetPixels( pix );
+        result.Apply();
+        return result;
+    }
+        private void drawBar(Rect rect, Color color)
+        {
+            InitStyles(color);
+            GUI.Box(rect, "",currentStyle );
+        }
+
+        private void InitStyles(Color color)
+    {
+        currentStyle = new GUIStyle( GUI.skin.box );
+        currentStyle.normal.background = MakeTex( 2, 2, color);
     }
 }
